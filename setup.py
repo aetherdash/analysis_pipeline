c820421fc2fb22e89d1f298849924df971acc1b3
bitbucket-pipelines <commits-noreply@bitbucket.org>
2022-05-04 20:40:04 +0000

    [skip ci] Updating version in setup.py [automated]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ NAME = "aether-analysis"
 DESCRIPTION='Utilities for the Analysis Pipeline'
 LICENSE='Aether Biomachines Private repo'
 REQUIRED = []
-VERSION = 0.10
+VERSION = 0.11
 
 setup(
     name=NAME,
